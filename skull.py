ace6f07a767e8efa98bb2e0656aceb100b1c4400
jesuSando <correoepicgames9@gmail.com>
2026-04-17 01:47:17 -0400

    feat: skull

---
+++ skull.py
@@ -0,0 +1,26 @@
+import time
+
+frames =[
+"""
+            _.,,,._
+       .,uS$$$$$$$SSI;,_
+     ,d$$S$$$$S$$$SS$SIi:.
+    J$S$$S7iIS$$S$$$S$SIi::
+  .d$$$7iIIS$$$$$$SS$$SIIi:.
+  $$$7:iIIS$S$$$SIS$$$$SIi::
+  j ?k:iIISIS$S7IS$$$SSIIi::
+.d? j$b:iIj$$S7IIS$$S7IIi::·
+`$$u$$7·:j$$SiiIS$$$SIIIi::·
+ biS$S$p,._?%u,`?S$SSIIi::·
+dS$?Ii?°?$S?°"`^"4ISIii::·
+°?$Sk.           ·?7I::·
+^:?i:"`         .-'¨
+
+""",
+
+]
+
+for i in range(len(frames)):
+      
+    print(frames[i])
+    time.sleep(1)
